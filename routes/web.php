2030311880bd6d109be42dd5cbf2f2162a8214d5
rohmet <harahmanabdarib@gmail.com>
2026-04-15 00:52:45 +0800

    feat: add keterlambatan routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Kelas\KeterlambatanController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,21 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'role:kelas'])->prefix('kelas')->name('kelas.')->group(function () {
+
+    Route::prefix('keterlambatan')->name('keterlambatan.')->group(function () {
+        // Halaman View
+        Route::get('/', [KeterlambatanController::class, 'index'])->name('index');
+        Route::get('/buat', [KeterlambatanController::class, 'create'])->name('create');
+        Route::post('/', [KeterlambatanController::class, 'store'])->name('store');
+        
+        // Endpoint AJAX
+        Route::get('/jadwal-hari-ini', [KeterlambatanController::class, 'jadwalHariIni'])->name('jadwal-hari-ini');
+        Route::get('/asisten/{idJadwal}', [KeterlambatanController::class, 'asisten'])->name('asisten');
+    });
+
 });
 
 require __DIR__.'/auth.php';
