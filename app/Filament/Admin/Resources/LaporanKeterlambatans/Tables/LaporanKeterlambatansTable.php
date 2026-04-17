92aafe67ccd6c46169230785c35ca50c3caa85e1
rohmet <harahmanabdarib@gmail.com>
2026-04-18 00:21:28 +0800

    feat: custom table for LaporanKeterlambatan

--- app/Filament/Admin/Resources/LaporanKeterlambatans/Tables/LaporanKeterlambatansTable.php
+++ app/Filament/Admin/Resources/LaporanKeterlambatans/Tables/LaporanKeterlambatansTable.php
@@ -6,6 +6,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class LaporanKeterlambatansTable
@@ -14,7 +15,41 @@ class LaporanKeterlambatansTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('waktu_lapor')
+                    ->label('Waktu Lapor')
+                    ->dateTime('d M Y, H:i')
+                    ->sortable(),
+
+                TextColumn::make('kelas.nama_kelas')
+                    ->label('Kelas Pelapor')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('jadwal.mataKuliah.nama_matkul')
+                    ->label('Mata Kuliah')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('asisten.nama_asisten')
+                    ->label('Nama Asisten')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('keterlambatan')
+                    ->label('Terlambat')
+                    ->suffix(' Menit')
+                    ->badge()
+                    ->color(fn (string $state): string => match (true) {
+                        $state == 0 => 'success',
+                        $state <= 15 => 'warning',
+                        default => 'danger',
+                    })
+                    ->sortable(),
+
+                TextColumn::make('deskripsi')
+                    ->label('Keterangan')
+                    ->limit(30)
+                    ->searchable(),
             ])
             ->filters([
                 //
@@ -25,8 +60,9 @@ class LaporanKeterlambatansTable
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
-                    DeleteBulkAction::make(),
+                    // DeleteBulkAction::make(), default ini tapi nd usah dipake
                 ]),
-            ]);
+            ])
+            ->defaultSort('waktu_lapor', 'desc');
     }
 }
