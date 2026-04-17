c052bbfdd7cafcb6ffbef44af9a74c14c689aca2
rohmet <harahmanabdarib@gmail.com>
2026-04-18 01:26:46 +0800

    feat: restrict /admin route access to Pak Adhy and super admin only

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,10 +10,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, HasRoles;
@@ -37,5 +39,14 @@ class User extends Authenticatable
     public function kelas()
     {
         return $this->hasOne(Kelas::class, 'id_user', 'id');
+    }
+
+    /**
+     * method membatasi akses filament /admin
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // role 'pak_adi' dan 'super_admin' yang bisa akses /admin
+        return $this->hasAnyRole(['pak_adi', 'super_admin']);
     }
 }
