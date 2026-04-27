660b89282b4addf55ba70509a0fa4faf41b09893
Napdoee <65323184+Napdoee@users.noreply.github.com>
2026-04-27 18:16:02 +0800

    fix: explicit index.php and view path

--- api/index.php
+++ api/index.php
@@ -1,2 +1,12 @@
-<?php  
-require __DIR__ . "/../public/index.php";
+<?php
+// Arahkan ke file autoload dan app di folder root
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Jalankan kernel Laravel
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
